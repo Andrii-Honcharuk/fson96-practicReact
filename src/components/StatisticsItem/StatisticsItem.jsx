88037cf1e899e6dbc4450daddad1328994d284fc
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-02-10 11:30:53 +0100

    task-2

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,15 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+
+import style from './Statistics.module.css';
+
+export const StatisticsItem = ({ total, title, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ size: '2em', color: 'orange' }}>
+        <div>{icon}</div>
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
