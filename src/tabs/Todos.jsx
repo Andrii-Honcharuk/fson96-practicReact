ea5010b80d8947ea24cfa92eedacf4813b4dc325
OlVin69 <aviniichuk@gmail.com>
2024-02-24 12:13:55 +0200

    add TodoList

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -2,19 +2,19 @@ import { Form, Text, TodoList } from 'components';
 import { useState } from 'react';
 
 export const Todos = () => {
-
   const [todos, setTodos] = useState([]);
 
-const handleSubmit = (newToodo) => {
-  setTodos((prevTodos)=> [...prevTodos, newToodo]);
-}
+  const handleSubmit = newToodo => {
+    setTodos(prevTodos => [...prevTodos, newToodo]);
+  };
 
   return (
     <>
       <Form onSubmit={handleSubmit} />
-      <TodoList />
-      <Text textAlign="center">There are no any todos ...</Text>
+      {todos.length > 0 && <TodoList todos={todos} />}
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
     </>
   );
-  
 };
