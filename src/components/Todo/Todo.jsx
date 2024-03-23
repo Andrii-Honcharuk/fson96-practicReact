baddff3cbcdf1ad5c601259ab66d9f815daab4d5
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-03-23 11:15:12 +0100

    add deleteTodo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,16 +1,28 @@
-import { Text } from "components";
-import style from "./Todo.module.css";
-import { RiDeleteBinLine, RiEdit2Line } from "react-icons/ri";
+import { Text } from 'components';
+import style from './Todo.module.css';
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from '../../redux/todosSlice';
 
 export const Todo = ({ todo, index }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(deleteTodo(todo.id));
+  };
+
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
-        TODO# {index + 1}
+        TODO #{index + 1}
       </Text>
 
       <Text>{todo.content}</Text>
-      <button className={style.deleteButton} type="button">
+      <button
+        className={style.deleteButton}
+        type="button"
+        onClick={handleClick}
+      >
         <RiDeleteBinLine size={24} />
       </button>
       <button className={style.editButton} type="button">
--- src/redux/todosSlice.js
+++ src/redux/todosSlice.js
@@ -17,11 +17,15 @@ const todosSlice = createSlice({
         };
       },
     },
+    deleteTodo: (state, { payload }) => {
+      const index = state.items.findIndex((item) => item.id === payload);
+      state.items.splice(index, 1);
+    },
   },
 });
 
-export const selectTodos = (state) => state.items
+export const selectTodos = (state) => state.items;
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, deleteTodo } = todosSlice.actions;
 const reducer = todosSlice.reducer;
 export default reducer;
